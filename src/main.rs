5b0b9f434a7d51fe999fe268d607a3587afefc62
thearjun <33110520+theArjun@users.noreply.github.com>
2025-03-01 23:00:22 +0545

    feat: convert into method

--- src/main.rs
+++ src/main.rs
@@ -1,24 +1,8 @@
-use chrono::NaiveDate;
-use csv::Reader;
-use serde::Deserialize;
-use std::error::Error;
+mod month_end;
 
-#[derive(Debug, Deserialize)]
-struct BSCalendar {
-    Baisakh: u8,
-    Jestha: u8,
-    Ashar: u8,
-    Shrawan: u8,
-    Bhadra: u8,
-    Asoj: u8,
-    Kartik: u8,
-    Mangsir: u8,
-    Poush: u8,
-    Magh: u8,
-    Falgun: u8,
-    Chaitra: u8,
-    Days: u16,
-}
+use chrono::NaiveDate;
+use month_end::BS_CALENDAR_MONTH_ENDS;
+use std::error::Error;
 
 struct BSDate {
     year: u16,
@@ -63,32 +47,8 @@ static MIN_NEPALI_DATE: (u16, u8, u8) = (1975, 1, 1);
 static MAX_NEPALI_DATE: (u16, u8, u8) = (2100, 12, 30);
 
 static START_ENGLISH_DATE: NaiveDate = NaiveDate::from_ymd_opt(1918, 4, 13).unwrap();
-static CALENDAR_PATH: &str = "calendar_bs.csv";
 
-fn get_bs_calendar_rows(path: &str) -> Result<Vec<BSCalendar>, Box<dyn Error>> {
-    let mut rdr = Reader::from_path(path)?;
-
-    let rows: Vec<BSCalendar> = rdr
-        .deserialize()
-        .skip(0) // Skip the header
-        .collect::<Result<Vec<BSCalendar>, csv::Error>>() // Collect the rows
-        .unwrap(); // Unwrap the result
-
-    Ok(rows)
-}
-
-fn get_digit_in_nepali(digit: u16) -> String {
-    let mut nepali_digit = String::new();
-    for d in digit.to_string().chars() {
-        nepali_digit.push_str(NEPALI_DIGITS[d.to_digit(10).unwrap() as usize]);
-    }
-    nepali_digit
-}
-
-fn main() -> Result<(), Box<dyn Error>> {
-    let rows = get_bs_calendar_rows(CALENDAR_PATH)?;
-    let ad_date = NaiveDate::from_ymd_opt(2025, 3, 1).unwrap();
-
+fn ad_to_bs(ad_date: NaiveDate) -> Result<BSDate, Box<dyn Error>> {
     // Algorithm to convert AD to BS
     // 1. Check if the date is within the range
     // 2. Check if the date is greater than 1918-04-13
@@ -98,11 +58,8 @@ fn main() -> Result<(), Box<dyn Error>> {
     // 6. Iterate over the rows and find the year
 
     if ad_date < START_ENGLISH_DATE {
-        println!("Date should be greater than 1918-04-13");
-        return Ok(()); // Return early
+        return Err("Date should be greater than 1918-04-13".into());
     }
-    let mut days_diff = ad_date.signed_duration_since(START_ENGLISH_DATE).num_days();
-    println!("Days diff: {}", days_diff);
 
     let mut bs_date = BSDate {
         year: 1975,
@@ -110,26 +67,15 @@ fn main() -> Result<(), Box<dyn Error>> {
         day: 1,
     };
 
-    for row in rows.iter() {
+    let mut days_diff = ad_date.signed_duration_since(START_ENGLISH_DATE).num_days();
+    println!("Days diff: {}", days_diff);
+
+    for row in BS_CALENDAR_MONTH_ENDS.iter() {
         if days_diff > 366 {
             bs_date.year += 1;
-            days_diff -= row.Days as i64;
+            days_diff -= row[row.len() - 1] as i64;
         } else {
-            let month_tuple: [u8; 12] = [
-                row.Baisakh,
-                row.Jestha,
-                row.Ashar,
-                row.Shrawan,
-                row.Bhadra,
-                row.Asoj,
-                row.Kartik,
-                row.Mangsir,
-                row.Poush,
-                row.Magh,
-                row.Falgun,
-                row.Chaitra,
-            ];
-
+            let month_tuple: &[u16] = &row[1..13];
             for (_, days) in month_tuple.iter().enumerate() {
                 if days_diff > *days as i64 {
                     days_diff -= *days as i64;
@@ -141,11 +87,20 @@ fn main() -> Result<(), Box<dyn Error>> {
             }
         }
     }
+    Ok(bs_date)
+}
 
-    println!(
-        "BS Date: {} {} {}",
-        bs_date.day, bs_date.month, bs_date.year
-    );
+fn get_digit_in_nepali(digit: u16) -> String {
+    let mut nepali_digit = String::new();
+    for d in digit.to_string().chars() {
+        nepali_digit.push_str(NEPALI_DIGITS[d.to_digit(10).unwrap() as usize]);
+    }
+    nepali_digit
+}
+
+fn main() -> Result<(), Box<dyn Error>> {
+    let ad_date = NaiveDate::from_ymd_opt(2025, 3, 1).unwrap();
+    let bs_date = ad_to_bs(ad_date)?;
 
     // Print the date in Nepali
     println!(
@@ -156,3 +111,11 @@ fn main() -> Result<(), Box<dyn Error>> {
     );
     Ok(())
 }
+
+// Algorithm to convert AD to BS
+// 1. Check if the date is within the range
+// 2. Check if the date is greater than 1918-04-13
+// 3. Calculate the difference between the date and 1918-04-13
+// 4. Calculate the number of days in between
+// 5. Add that number of days to the start of the BS calendar
+// 6. Iterate over the rows and find the year
